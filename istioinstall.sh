49e12793837c05867c1521ed48354d4dab511f84
Ghoulli <58692950+Ghoulli@users.noreply.github.com>
2025-05-27 14:18:38 +0200

    Update istioinstall.sh

--- istioinstall.sh
+++ istioinstall.sh
@@ -5,7 +5,7 @@ K3S_BINARY="/usr/local/bin/k3s"
 
 if [ -x "$K3S_BINARY" ]; then
 	curl -L https://istio.io/downloadIstio | sh -
-	cd istio-1.25.1
+	cd istio-1.26.0
 	export PATH=$PWD/bin:$PATH
 	istioctl install -f samples/bookinfo/demo-profile-no-gateways.yaml -y
 	kubectl label namespace default istio-injection=enabled
